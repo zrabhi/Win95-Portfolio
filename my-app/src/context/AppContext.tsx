9df759c7517ff415e836fd44504fee83b42cbf6e
zrabhi <zac.rabhi12@gmail.com>
2024-05-11 19:12:11 +0100

    minor changes

--- my-app/src/context/AppContext.tsx
+++ my-app/src/context/AppContext.tsx
@@ -1,46 +1,57 @@
 import React, { useState } from "react";
 
 interface BiographyType {
-    isBioOpen: boolean
-    isBioResumed : boolean
-    setIsBioOpen: (Open: boolean) => void;
-    setIsBioResumed: (Resumed: boolean) => void;
+  isBioOpen: boolean;
+  isBioResumed: boolean;
+  setIsBioOpen: (Open: boolean) => void;
+  setIsBioResumed: (Resumed: boolean) => void;
 }
 
 //TODO: Resume functionality to REsume window
-interface ResumeType{
-    isResumeOpen: boolean
-    setIsResumeOpen: (Open: boolean) => void;
+interface ResumeType {
+  isResumeOpen: boolean;
+  isResumeResumed: boolean;
+  setIsResumeOpen: (Open: boolean) => void;
+  setIsResumeResumed: (Resumed: boolean) => void;
 }
 
-interface AppContextType
-{
-    BioData: BiographyType
-    ResumeData: ResumeType
+interface AppContextType {
+  BioData: BiographyType;
+  ResumeData: ResumeType;
 }
 interface AppProviderProps {
-    children : React.ReactNode
+  children: React.ReactNode;
 }
 
-export const AppContext = React.createContext<AppContextType | null>(null)
+export const AppContext = React.createContext<AppContextType | null>(null);
 
+export const AppProvider: React.FC<AppProviderProps> = ({ children }) => {
+  const [biography, setBiography] = useState<BiographyType>({
+    isBioOpen: true,
+    isBioResumed: false,
+    setIsBioOpen: (Open: boolean) =>
+      setBiography((prevState: BiographyType) => ({
+        ...prevState,
+        isBioOpen: Open,
+      })),
+    setIsBioResumed: (Resumed: boolean) =>
+      setBiography((prevState: BiographyType) => ({
+        ...prevState,
+        isBioResumed: Resumed,
+      })),
+  });
 
-export const AppProvider : React.FC<AppProviderProps>  = ({ children }) =>{
-    const [biography, setBiography] = useState<BiographyType>({
-        isBioOpen: true,
-        isBioResumed: false,
-        setIsBioOpen: (Open : boolean) => setBiography((prevState: BiographyType) => ({...prevState, isBioOpen: Open})),
-        setIsBioResumed: (Resumed: boolean) => setBiography((prevState: BiographyType) => ({...prevState, isBioResumed: Resumed})),
-    })
-
-    const [resume, setResume] = useState<ResumeType>({
-        isResumeOpen: false,
-        setIsResumeOpen: (Open : boolean) => setResume(prevState => ({...prevState, isResumeOpen: Open}))
-    })
-    return (
-        <AppContext.Provider value={{BioData: biography, ResumeData: resume }}>
-            {children}
-        </AppContext.Provider>
-
-    )
-}
+  const [resume, setResume] = useState<ResumeType>({
+    isResumeOpen: false,
+    isResumeResumed: false,
+    setIsResumeOpen: (Open: boolean) =>
+      setResume((prevState : ResumeType) => ({ ...prevState, isResumeOpen: Open })),
+    setIsResumeResumed: (Resumed: boolean) =>
+        setResume((prevState : ResumeType) => ({...prevState, isResumeResumed: Resumed}))
+  });
+  return (
+    <AppContext.Provider value={{ BioData: biography, ResumeData: resume }}>
+      {children}
+    </AppContext.Provider>
+  );
+};
